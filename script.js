9a8f8460b93703e1322e6e16588c8f2e7b4875cb
fezilengoman18-art <fezilengoman18@gmail.com>
2026-06-12 23:45:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.createElement("button");
+
+button.innerText = "🌙";
+
+button.className = "theme-btn";
+
+document.body.appendChild(button);
+
+button.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
+window.addEventListener("load", () => {
+    document.getElementById("loader").style.display = "none";
+});
